655e224e168e664c7a00c01f234ffa43f6cbd94e
Sebastian Estay <sebastian@HP-ENVY>
2019-11-24 23:41:37 +0100

    final

--- 02 tic-tac-toe/tic-tac-toe.py
+++ 02 tic-tac-toe/tic-tac-toe.py
@@ -3,23 +3,33 @@ import itertools
 
 def win(current_game):
 
+    def all_same(l):
+        if l.count(l[0]) == len(l) and l[0] != 0:
+            return True
+        else:
+            return False
+
     # horizontal
     for row in game:
-        if row.count(row[0]) == len(row) and row[0] != 0:
+        print(row)
+        if all_same(row):
             print(f"Player {row[0]} is the winner horizontally")
+            return True
 
     # diagonal
     diags = []
     for col, row in enumerate(reversed(range(len(game)))):
         diags.append(game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally (/)")
+        return True
 
     diags = []
     for ix in range(len(game)):
         diags.append(game[ix][ix])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally (\\)")
+        return True
 
     # vertical
     for col in range(len(game)):
@@ -28,22 +38,32 @@ def win(current_game):
         for row in game:
             check.append(row[col])
 
-        if check.count(check[0]) == len(check) and check[0] != 0:
+        if all_same(check):
             print(f"Player {check[0]} is the winner vertically")
+            return True
+
+    return False
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
     try:
-        print("   0  1  2")
+        if game_map[row][column] != 0:
+            print("This position is ocupado! Choose another!")
+            return game_map, False
+        print("   "+"  ".join([str(i) for i in range(len(game_map))]))
         if not just_display:
             game_map[row][column] = player
         for count, row in enumerate(game_map):
             print(count, row)
-        return game_map
+        return game_map, True
+
     except IndexError as e:
         print("Error: make sure you input row/column as 0, 1 or 2?", e)
+        return game_map, False
+
     except Exception as e:
         print("Something went very wrong,", e)
+        return game_map, False
 
 
 play = True
@@ -54,21 +74,29 @@ while play:
             [0, 0, 0]]
 
     game_won = False
-    game = game_board(game, just_display=True)
+    game, _ = game_board(game, just_display=True)
     player_choice = itertools.cycle([1, 2])
     while not game_won:
         current_player = next(player_choice)
         print(f"Current Player: {current_player}")
-        column_choice = int(input("What column do you want to play? (0, 1, 2): "))
-        row_choice = int(input("What row do you want to play? (0, 1, 2): "))
-        game = game_board(game, current_player, row_choice, column_choice)
+        played = False
 
+        while not played:
+            column_choice = int(input("What column do you want to play? (0, 1, 2): "))
+            row_choice = int(input("What row do you want to play? (0, 1, 2): "))
+            game, played = game_board(game, current_player, row_choice, column_choice)
 
-
-
-
-
-
+        if win(game):
+            game_won = True
+            again = input("The game is over, would you like to play again? (y/n) ")
+            if again.lower() == "y":
+                print("Restarting")
+            elif again.lower() == "n":
+                print("Bye")
+                play = False
+            else:
+                print("Not a valid answer")
+                play = False
 
 
 
